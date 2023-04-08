361c77f27bf4a2677cecc1f90b1958ca0c9abf64
dell <saharysaleh2000@gmail.com>
2023-04-08 14:58:25 +0300

    added Features Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from "./components/Card"
+import Features from "./components/Features"
 function App() {
   return (
     <div >
       <Card/>
+      <Features/>
     </div>
   );
 }
