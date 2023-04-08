57a7c0c2ebfaf784da92c3e2b56c9f3dbd8e7101
dell <saharysaleh2000@gmail.com>
2023-04-08 15:44:39 +0300

    carts now dynamic

--- public/productsData.js
+++ public/productsData.js
@@ -1,4 +1,4 @@
-export const productsData = [
+ const productsData = [
   {
     "id": 1,
     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
@@ -240,3 +240,4 @@ export const productsData = [
     }
   }
 ];
+export default productsData
--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Router from "./Router.js";
+
+// function App() {
+//   return <Router />;
+// }
+
+// export default App;
+
 import logo from "./logo.svg";
 import "./App.css";
 import Router from "./Router.js";
+import  Card from "./components/Card";
 
 function App() {
-  return <Router />;
+  return <Card />;
 }
 
 export default App;
