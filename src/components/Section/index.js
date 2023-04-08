b4d6d08c28f98cb6f6fa46cd212012e95601890e
Mosab Dwaikat <mosabdwaikat@gmail.com>
2023-04-08 15:52:41 +0300

    v2

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import styles from "./Section.module.css";
 import Subtitle from "./Subtitle";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { Stack } from "@mui/material";
+import Card from "../Card";
 import Title from "./Title";
+import { Stack } from "@mui/material";
 const Section = () => {
   return (
-    <Box sx={{ p: 2, height: 493 + "px", border: "1px dashed grey" }}>
+    <Box sx={{ p: 2, border: "1px solid grey" }}>
       <Subtitle />
       <Title />
+      <Stack direction="row" spacing={3.75}>
+        {arr.map((e) => {
+          return <Card />;
+        })}
+      </Stack>
     </Box>
   );
 };
 
 export default Section;
+const arr = [{}, {}, {}, {}];
