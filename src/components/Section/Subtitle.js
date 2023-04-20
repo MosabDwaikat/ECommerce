4e4d734da1cac1a399f03ca0cfaf615a46ed91ce
MosabDwaikat <114504754+MosabDwaikat@users.noreply.github.com>
2023-04-20 23:09:29 +0300

    Revert "Sahar"

--- src/components/Section/Subtitle.js
+++ src/components/Section/Subtitle.js
@@ -4,19 +4,19 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Stack } from "@mui/material";
 
-const Subtitle = (props) => {
+const Subtitle = () => {
   return (
     <Stack direction="row" spacing={2} alignItems="center">
       <Box className={styles.subtitleBox} />
       <Typography
         fontFamily={"Poppins"}
         fontStyle={"normal"}
-        fontWeight={900}
+        fontWeight={600}
         fontSize={16 + "px"}
         lineHeight={20 + "px"}
         color={"#DB4444"}
       >
-       {props.subTitle}
+        subtitle
       </Typography>
     </Stack>
   );
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -5,14 +5,11 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Stack } from "@mui/material";
 import Title from "./Title";
-import ActionAreaCard from "../Card";
-const Section = (props) => {
+const Section = () => {
   return (
-    <Box sx={{ p: 5, height: 60 + "px", border: "1px  grey" }}>
-      <Subtitle subTitle={props.subTitle} />
-      <Title title={props.title}time={props.time}/>
-     
-     
+    <Box sx={{ p: 2, height: 493 + "px", border: "1px dashed grey" }}>
+      <Subtitle />
+      <Title />
     </Box>
   );
 };
