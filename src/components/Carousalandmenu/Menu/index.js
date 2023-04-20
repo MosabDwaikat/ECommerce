4b0c7e8b05c9a0fc51d9ac855ad55f52c4dfe223
ahmad amira <ahmad_amira@outlook.com>
2023-04-20 20:42:31 +0300

    added carousel and menu

---
+++ src/components/Carousalandmenu/Menu/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const elements = [
+    { label: 'Woman’s Fashion' },
+    { label: 'Men’s Fashion' },
+    { label: 'Electronics' },
+    { label: 'Home & Lifestyle' },
+    { label: 'Medicine' },
+    { label: 'Sports & Outdoor' },
+    { label: 'Baby’s & Toys' },
+    { label: 'Groceries & Pets' },
+    { label: 'Health & Beauty' },
+];
+
+const MyBox = () => {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', p: 2 }}>
+            {elements.map((element, index) => (
+                <Box
+                    key={index}
+                    sx={{
+                        position: 'relative',
+                        display: 'flex',
+                        alignItems: 'left',
+                        justifyContent: 'left',
+
+
+                        height: '24px',
+                        bgcolor: element.color,
+                        mb: 2 // set my property to 2 for 16px vertical margin
+                    }}>
+                    <Typography
+                        variant="h5"
+                        color="white"
+                        sx={{
+                            fontFamily: 'Poppins',
+                            fontStyle: 'normal',
+                            marginRight: '20px',
+                            fontWeight: 400,
+                            fontSize: '16px',
+                            lineHeight: '24px',
+                            textAlign: 'center',
+                            color: '#000000',
+                        }}
+                    >
+
+                        {element.label}
+                        {index == 0 || index == 1 ? <ArrowForwardIosIcon /> : <></>}
+
+                    </Typography>
+
+                </Box>
+            ))}
+        </Box>
+    );
+};
+
+export default MyBox;
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Typography,Stack,Rating,Divider} from "@mui/material";
+import { Typography, Stack, Rating, Divider } from "@mui/material";
 
 import Product from "../product";
 import Section from "../Section";
@@ -9,27 +9,29 @@ import TimeRemaining from "../Section/TimeRemaining";
 import ActionAreaCard from "../Card";
 import GridCards from "../Card/GridCards";
 import Features from "../Features";
+import Carousalandmenu from "../Carousalandmenu";
 
 const HomePage = () => {
   return (
 
-    
-      < Stack direction="column" spacing={4} sx={{ mt: 1,ml:7}}>
-      <>carousal and menu </>
-      <Section title="Flash Sales" subTitle="today's" time={<TimeRemaining /> }/>
+
+    < Stack direction="column" spacing={4} sx={{ mt: 1, ml: 7 }}>
+      <Divider orientation="horizontal" flexItem />
+      <Carousalandmenu />
+      <Section title="Flash Sales" subTitle="today's" time={<TimeRemaining />} />
       <ActionAreaCard />
       <>button</>
-      <Divider  width={"80%"} sx={{m:8}} />
+      <Divider width={"80%"} sx={{ m: 8 }} />
       <Section title="Browse By Category" subTitle="catalog" time={<>arrows component</>} />
       <>button's caltalog</>
-      <Section title="Best Selling Products" subTitle="This Month" time={<>button</>}/>
-      <RelatedItems path="/products/:id"/>
-      <Divider  width={"80%"} sx={{m:8}} />
+      <Section title="Best Selling Products" subTitle="This Month" time={<>button</>} />
+      <RelatedItems path="/products/:id" />
+      <Divider width={"80%"} sx={{ m: 8 }} />
       <> caltalog with image </>
-      <Section title="Explore Our Products" subTitle="Our Products" time={<>arrows component</> }/>
-      <GridCards/>
+      <Section title="Explore Our Products" subTitle="Our Products" time={<>arrows component</>} />
+      <GridCards />
       <>button</>
-      <Divider  width={"80%"} sx={{m:8}} />
+      <Divider width={"80%"} sx={{ m: 8 }} />
       <Section title="Featured" subTitle="New Arrival" />
       <>images for new arrival</>
       <Features />
@@ -41,7 +43,7 @@ const HomePage = () => {
 
 
 
-      </Stack>
+    </Stack>
 
   );
 };
