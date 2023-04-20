4981999b68612b48092b4170b7a0178f34c8b9bd
Mosab Dwaikat <mosabdwaikat@gmail.com>
2023-04-20 23:18:03 +0300

    try 3

--- src/Router.js
+++ src/Router.js
@@ -11,31 +11,25 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-          
-        <Route path="/" element={<Layout />}>
-<<<<<<< HEAD
-          <Route
-            index
-            element={
-              <Section
-                title={"Title of section"}
-                subtitle={"subtitle"}
-                cardsViewed={4}
-                rows={2}
-                viewTime={true}
-                viewBottomButton={true}
-                topRightCorner={"slider-buttons"} //slider-buttons or button
-                products={products}
-              />
-            }
-          />
-=======
-          <Route index element={<HomePage />} />
-          <Route path="/products" element={<ActionAreaCard />} />
-          <Route path="/products/:id" element={<SecondPage />} />
+        <Route index element={<HomePage />} />
+        <Route path="/products" element={<ActionAreaCard />} />
+        <Route path="/products/:id" element={<SecondPage />} />
 
->>>>>>> 1a17fc7dcb5a36a0345d0db3c6dc3a34b74126a8
-        </Route>
+        <Route
+          path="section"
+          element={
+            <Section
+              title={"Title of section"}
+              subtitle={"subtitle"}
+              cardsViewed={4}
+              rows={2}
+              viewTime={true}
+              viewBottomButton={true}
+              topRightCorner={"slider-buttons"} //slider-buttons or button
+              products={products}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
